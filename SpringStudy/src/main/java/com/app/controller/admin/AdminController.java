19ca7ac9757d08c0abc381c8ce29f23e7944e7ee
YuYoungKwang <db99817@gmail.com>
2026-02-05 11:45:36 +0900

    DB연동 Room 데이터 Insert 저장, Mybatis-config

--- SpringStudy/src/main/java/com/app/controller/admin/AdminController.java
+++ SpringStudy/src/main/java/com/app/controller/admin/AdminController.java
@@ -1,13 +1,21 @@
 package com.app.controller.admin;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.app.dto.room.Room;
+import com.app.dto.user.User;
 import com.app.service.room.RoomService;
+import com.app.service.user.UserService;
 
 @Controller
 public class AdminController {
@@ -16,6 +24,8 @@ public class AdminController {
 	@Autowired
 	RoomService roomService;
 	
+	@Autowired
+	UserService userService;
 	
 	@GetMapping("/admin/registerRoom")
 	public String registerRoom() {
@@ -31,8 +41,128 @@ public class AdminController {
 		int result = roomService.saveRoom(room);
 		//result 값 
 		// 성공? 실패? -> 진행 방향
+		System.out.println("insert 처리 결과 적용 행의 수 : " + result);
 		
-		return "admin/registerRoom";
+		//저장 성공 or 실패
+		if(result > 0 ) { //저장 성공
+			return "redirect:/admin/rooms";
+		} else { //저장 실패 
+			return "admin/registerRoom";	
+		}
 	}
 	
+	
+	
+	//관리자가 객실 관리   전체 객실 목록 조회
+	@GetMapping("/admin/rooms")
+	public String rooms(Model model) {
+		
+		//rooms 페이지 정보
+		// T_ROOM 객실데이터   DB 조회
+		
+		List<Room> roomList = roomService.findRoomList();
+		
+		model.addAttribute("roomList", roomList);
+		
+		return "admin/rooms";		
+	}
+	
+	//객실 개별 상세 페이지 조회
+	//@GetMapping("/admin/room?roomId=3")
+	@GetMapping("/admin/room/{roomId}")
+	public String room(@PathVariable String roomId, Model model) {
+		
+		Room room = roomService.findRoomByRoomId( Integer.parseInt(roomId) );
+		model.addAttribute("room",room);
+		
+		if(room == null) { //roomId 에 해당하는 정보가 없다
+			//조회가 정보가 없을때 보여줄 페이지
+		}
+		
+		return "admin/room";
+	}
+	
+	//객실 정보 삭제
+	//localhost:8080/admin/removeRoom?roomId=4
+	@GetMapping("/admin/removeRoom")
+	public String removeRoom(HttpServletRequest request) {
+		String roomId = request.getParameter("roomId");
+		
+		if( roomId == null ) {
+			return "redirect:/admin/rooms";
+		}
+		
+		int result = roomService.removeRoom( Integer.parseInt(roomId) );
+		
+		//if(result > 0)
+		
+		return "redirect:/admin/rooms";
+	}
+	
+	
+	
+	
+	//관리자가 사용자계정관리 -> 사용자 계정 추가
+	@GetMapping("/admin/users/add")
+	public String addUser() {
+		return "admin/addUser";
+	}
+	
+	@PostMapping("/admin/users/add")
+	public String addUser(User user) {
+		
+		//user 정보를 DB에 저장
+		System.out.println(user);
+		
+		//관리자가 사용자 계정을 추가
+		//사용자 계정 -> userType 값은 "CUS" 코드로 저장되어야한다.
+		
+		/*
+		1) 컨트롤러에서 바로 처리
+		
+		user.setUserType("CUS");
+		
+		userService.saveUser(user);
+		 */
+		
+		/*
+		2) 서비스 계층/레이어/레벨 에서 사용자를 저장하는 메소드를 활용 
+		
+		userService.saveCustomerUser(user);
+		 */
+		
+		int result = userService.saveCustomerUser(user);
+		
+		if(result > 0) {
+			return "redirect:/admin/users";
+		} else {
+			return "admin/addUser";	
+		}
+	}
+	
+	@GetMapping("/admin/users")
+	public String users(Model model) {
+		List<User> userList = userService.findUserList();
+		model.addAttribute("userList", userList);
+		
+		return "admin/users";
+	}
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- SpringStudy/src/main/java/com/app/dao/room/impl/RoomDAOImpl.java
+++ SpringStudy/src/main/java/com/app/dao/room/impl/RoomDAOImpl.java
@@ -2,6 +2,8 @@ package com.app.dao.room.impl;
 
 import java.util.List;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.app.dao.room.RoomDAO;
@@ -15,14 +17,19 @@ import com.app.dto.room.Room;
 @Repository
 public class RoomDAOImpl implements RoomDAO {
 
+	//실제 DB 연동 처리할떄 필요한 관련 객체(Bean)
+	@Autowired
+	SqlSessionTemplate sqlSessionTemplate; 
+	
 	@Override
 	public List<Room> findRoomList() {
 		
 		System.out.println("[DAO] 호출 findRoomList");
 		
 		// DB 에 연동 -> room 테이블 데이터 조회 -> List
+		List<Room> roomList = sqlSessionTemplate.selectList("room_mapper.findRoomList");
 		
-		return null;
+		return roomList;
 	}
 
 	@Override
@@ -30,7 +37,28 @@ public class RoomDAOImpl implements RoomDAO {
 
 		//DB 에 room 정보 저장 
 		
-		return 0;
+							//실행할 쿼리가 위치한 식별자    ,   매개변수
+							//namespace.id
+		int result = sqlSessionTemplate.insert("room_mapper.saveRoom", room);
+		
+		return result;
+	}
+
+	@Override
+	public Room findRoomByRoomId(int roomId) {
+
+		Room room = sqlSessionTemplate.selectOne("room_mapper.findRoomByRoomId", roomId);
+		
+		return room;
+	}
+
+	@Override
+	public int removeRoom(int roomId) {
+
+		//delete 삭제 쿼리 -> return 적용된 행의 갯수
+		int result = sqlSessionTemplate.delete("room_mapper.removeRoom", roomId);
+		
+		return result;
 	}
 
 }
