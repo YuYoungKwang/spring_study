369e9cf2f603ca37bc3207646c15c89f4c48030e
YuYoungKwang <db99817@gmail.com>
2026-02-05 11:46:36 +0900

    관리자페이지 사용자 계정 추가하기, jdbc Null 처리 설정

--- SpringStudy/src/main/java/com/app/service/user/impl/UserServiceImpl.java
+++ SpringStudy/src/main/java/com/app/service/user/impl/UserServiceImpl.java
@@ -1,14 +1,66 @@
 package com.app.service.user.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.app.common.CommonCode;
 import com.app.dao.user.UserDAO;
+import com.app.dto.user.User;
 import com.app.service.user.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
-	
+
 	@Autowired
 	UserDAO userDAO;
+
+	@Override
+	public int saveUser(User user) {
+
+		int result = userDAO.saveUser(user);
+
+		return result;
+	}
+
+	@Override
+	public int saveCustomerUser(User user) {
+		// 사용자 계정 추가시 사용하는 메소드
+		// 사용자 추가 로직
+		//user.setUserType("CUS");
+		user.setUserType( CommonCode.USER_USERTYPE_CUSTOMER );
+		
+		int result = userDAO.saveUser(user);
+
+		return result;
+	}
+
+	@Override
+	public int saveAdminUser(User user) {
+		// 관리자 계정 추가시 사용하는 메소드
+		// 관리자 추가 로직
+		//user.setUserType("ADM");
+		user.setUserType( CommonCode.USER_USERTYPE_ADMIN );
+		
+		int result = userDAO.saveUser(user);
+
+		return result;
+	}
+
+	@Override
+	public List<User> findUserList() {
+
+		List<User> userList = userDAO.findUserList();
+		
+		return userList;
+	}
 }
+
+
+
+
+
+
+
+
